f8304a74339302cd0eceef0f865d1065a0520157
jorge <jorgealexiscano@gmail.com>
2021-03-25 22:25:19 -0300

    resulucion de ejercicios para corregir

---
+++ ejercicios.py
@@ -0,0 +1,44 @@
+def superficie_rectangulo(b,h):
+    superficie = b*h
+    return superficie
+
+base = float(input('ingresar la base del rectangulo: '))
+altura = float(input('ingreasar la altura del rectangulo: '))
+resultado = superficie_rectangulo(base,altura)
+
+print(f'La superficie del rectangulo es iguala {resultado}.') 
+
+
+def numero in numero == 1:
+       return False 
+    elif numero == 2:
+        return True
+    else:
+        for i in range(2,numero):
+            if numero % i == 0:
+                return False
+        return True
+
+for i in range (1,101):
+    print(i, es_primo(i))  
+ 
+ 
+cadena1= input ('ingresar una cadena')
+
+cadena_al_reves = cadena1[::-1]
+if(cadena1 == cadena_al_reves):
+    print('es palindromo')
+else:
+    print('no es palindromo')
+
+
+def superficie_triangulorectangulo(b,h):
+    superficie = b*h/2
+    return superficie
+base = float(input('ingresar la base del triangulorectangulo: '))
+altura = float(input('ingresar la altura del triangulorectangulo:'))
+resultado = superficie_triangulorectangulo(base,altura)
+
+print(f'La superficie del triangulorectangulo es igual a {resultado}')
+   
+
